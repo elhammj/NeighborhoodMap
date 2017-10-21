5bab78e401047efc98efe1870d5b1f9d4ddb0024
Elham <elham@localhost>
2017-10-21 03:43:43 +0300

    fixed issues

--- js/app.js
+++ js/app.js
@@ -16,29 +16,29 @@ var infoWindow;
 */
 function initMap() {
 		
-		map = new google.maps.Map(document.getElementById('map'), {
-          center: locations[6].location,
-          zoom: 12,
-          styles: mapStyle,
-          mapTypeControl: true,
-	      mapTypeControlOptions: {
-	          style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-	          position: google.maps.ControlPosition.TOP_CENTER
-	      },
-	      zoomControl: true,
-	      zoomControlOptions: {
-	          position: google.maps.ControlPosition.LEFT_CENTER
-	      },
-	      scaleControl: true,
-	      streetViewControl: true,
-	      streetViewControlOptions: {
-	          position: google.maps.ControlPosition.LEFT_TOP
-	      },
-	      fullscreenControl: true
-        });
+	map = new google.maps.Map(document.getElementById('map'), {
+    center: locations[6].location,
+    zoom: 12,
+    styles: mapStyle,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+        position: google.maps.ControlPosition.TOP_CENTER
+    },
+    zoomControl: true,
+    zoomControlOptions: {
+        position: google.maps.ControlPosition.LEFT_CENTER
+    },
+    scaleControl: true,
+    streetViewControl: true,
+    streetViewControlOptions: {
+        position: google.maps.ControlPosition.LEFT_TOP
+    },
+    fullscreenControl: true
+  });
 
-        this.AddMarker(); //To add marker to each location as a key and value
-        ko.applyBindings(new LocationsViewModel()); //Binding the view model 
+  this.AddMarker(); //To add marker to each location as a key and value
+  ko.applyBindings(new LocationsViewModel()); //Binding the view model 
 }
     
 
@@ -63,31 +63,32 @@ var LocationsViewModel = function () {
 	    var filter = self.filter();
 	    if (filter) {
 	    	//Filter the list
-	        ko.utils.arrayForEach(self.viewLocations(), function (element) {
-	        	var currentTitle = element.title.toLowerCase();
-	        	var currentText = filter.toLowerCase();
-	        	//Search allows the user to find a locaiton that contains this keyword
-	            if (currentTitle.indexOf(currentText) !== -1) { 
-	                copyArr.push(element);
-	                element.marker.setVisible(true);
-	            }
-	            else{
-	            	element.marker.setVisible(false);//set false to hide the marker during filtering 
-	            }
-	        });
+        ko.utils.arrayForEach(self.viewLocations(), function (element) {
+        	var currentTitle = element.title.toLowerCase();
+        	var currentText = filter.toLowerCase();
+        	//Search allows the user to find a locaiton that contains this keyword
+            if (currentTitle.indexOf(currentText) !== -1) { 
+                copyArr.push(element);
+                element.marker.setVisible(true);
+            }
+            else{
+            	element.marker.setVisible(false);//set false to hide the marker during filtering 
+            }
+        });
 	    } 
 	    else {
-	        copyArr = self.viewLocations(); //Return the regular markers and locations
-			 ko.utils.arrayForEach(self.viewLocations(), function (element) {
-			 	element.marker.setVisible(true);
-      		 });
+        copyArr = self.viewLocations(); //Return the regular markers and locations
+        ko.utils.arrayForEach(self.viewLocations(), function (element) {
+        element.marker.setVisible(true);
+        });
 	    } 
 	    //Indicate which title has been selected and show infowwindow with animating the marker by calling AddAnimation function 
 	    self.locationClicked = function(element) {
 	    	if (element.marker.getAnimation() !== null) {
-	            element.marker.setAnimation(null);
-	        } else {
-	        	map.panTo(element.marker.position); //Zoom to the current marker, when it is clicked  
+          element.marker.setAnimation(null);
+	       }
+         else {
+	        map.panTo(element.marker.position); //Zoom to the current marker, when it is clicked  
 	  			AddInfo(element.marker, infoWindow);
 	  			AddAnimation(element.marker,"FFFFFF");
 	  		}
@@ -111,28 +112,28 @@ var AddMarker = function() {
 	//Action when the user click on a pin, animate and change the color by calling AddInfo and AddAnimation functions
 	function AddAnimationAndInfo(marker) {
   	return function(){
-      	map.panTo(marker.position); //Zoom to the current marker, when it is clicked   
-		AddAnimation(marker,"FFDF4C");
-		AddInfo(marker, infoWindow); 
+      map.panTo(marker.position); //Zoom to the current marker, when it is clicked   
+  		AddAnimation(marker,"FFDF4C");
+  		AddInfo(marker, infoWindow); 
 	  };
 	}
 
-    // Get the position and title from the location array.
-   	for (var i=0; i< locations.length; i++)
-   	{
-   		var position = locations[i].location;
-   		var title = locations[i].title;
-   		var formatAddress = locations[i].address;
-      	marker = new google.maps.Marker({
-      	position:new google.maps.LatLng(position.lat, position.lng), 
-      	map: map, 
-      	address: formatAddress,
-      	title:title, 
-      	visible: true,
-      	icon: iconUrl });
-      	marker.addListener("click", AddAnimationAndInfo(marker), false); //Add Info and Animation to each marker
-    	locations[i].marker = marker; //Add a marker to the locaiton object as an attribute
-	} 
+  // Get the position and title from the location array.
+ 	for (var i=0; i< locations.length; i++)
+ 	{
+ 		var position = locations[i].location;
+ 		var title = locations[i].title;
+ 		var formatAddress = locations[i].address;
+      marker = new google.maps.Marker({
+      position:new google.maps.LatLng(position.lat, position.lng), 
+      map: map, 
+      address: formatAddress,
+      title:title, 
+      visible: true,
+      icon: iconUrl });
+    marker.addListener("click", AddAnimationAndInfo(marker), false); //Add Info and Animation to each marker
+  	locations[i].marker = marker; //Add a marker to the locaiton object as an attribute
+  } 
 };
 
 
@@ -157,15 +158,16 @@ function AddInfo(marker, infoWindow){
       //Wikipedia API returns undefined 
       if (wikiLocURL === undefined) {
         infoWindow.setContent('<div id="info">' + '<h6>' + marker.title + '</h6></br>' + '<h8>' + marker.address + '</h8> </br></br> <p>' + '*** No Wikipedia data for this location ***' + '</p> </br> <a href="https://en.wikipedia.org/wiki/Main_Page" target="blank">' + 'Visit Wikipedia to Search' + '</a> </p> </div>');
-      } else { //when it is successful and data returned
+      } 
+      else { //when it is successful and data returned
         infoWindow.marker = marker;
         infoWindow.setContent('<div id="info">' + '<h6>' + marker.title + '</h6></br>' + '<h8>' + marker.address + '</h8> </br> </br> <p>' + wikiContent + "...." + '</p> </br> <a href="' + wikiLocURL + '" target="blank">' + 'Visit Wikipedia to Read More' + '</a> </p> </div>');
       }
       infoWindow.open(map, marker);
       //When Wikipedia Return 
       }).error(function() {
-      infoWindow.setContent('<div id="info">' + '<h6>' + marker.title + '</h6></br>' + '<h8>' + marker.address + '</h8> </br> </br> <p>' + '*** Error while retreving wikipedia data ***' + '</p> </br> <a href=href="https://en.wikipedia.org/wiki/Main_Page" target="blank">' + 'Visit Wikipedia' + '</a> </p> </div>');
-      infoWindow.open(map, marker);
+        infoWindow.setContent('<div id="info">' + '<h6>' + marker.title + '</h6></br>' + '<h8>' + marker.address + '</h8> </br> </br> <p>' + '*** Error while retreving wikipedia data ***' + '</p> </br> <a href=href="https://en.wikipedia.org/wiki/Main_Page" target="blank">' + 'Visit Wikipedia' + '</a> </p> </div>');
+        infoWindow.open(map, marker);
 	});
 }
 
@@ -174,16 +176,17 @@ function AddInfo(marker, infoWindow){
 */
 
 function AddAnimation(marker,color){
-	 if (marker.getAnimation() !== null) {
-            marker.setAnimation(null);
-	        } else {
-	        	marker.setAnimation(google.maps.Animation.BOUNCE);
-           		marker.setIcon('https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + color);
-	            setTimeout(function () {
-				    marker.setAnimation(null);
-				    marker.setIcon(iconUrl);
-				}, 700);
-	        }
+ if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  }
+  else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+    marker.setIcon('https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + color);
+    setTimeout(function () {
+    marker.setAnimation(null);
+    marker.setIcon(iconUrl);
+    }, 700);
+  }
 }
 	
 
@@ -199,13 +202,12 @@ function handleError(){
 */
 function showSideBar() {
 	if (document.getElementById('sidebar').style.display == 'none'){
-			document.getElementById('sidebar').style.width = "30%";
-		   document.getElementById('sidebar').style.display = "block";
-		   document.getElementById('mapCol').style.width = "70%";
+    document.getElementById('sidebar').style.width = "30%";
+    document.getElementById('sidebar').style.display = "block";
+    document.getElementById('mapCol').style.width = "70%";
 	}
 	else{
-		   document.getElementById('sidebar').style.display = "none";
-		   document.getElementById('mapCol').style.width = "100%";
-
+    document.getElementById('sidebar').style.display = "none";
+    document.getElementById('mapCol').style.width = "100%";
 	}
 }
